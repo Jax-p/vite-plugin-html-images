a9ff6d98ff6d9e8ef7ff5e41bc9325eb779a9ec5
Jakub Patočka <jaxpcze@gmail.com>
2021-12-17 13:22:30 +0100

    fix: Options are optional

--- src/types.ts
+++ src/types.ts
@@ -9,11 +9,11 @@ export interface ImageOptions
      * The temporary directory must be inside the source directory (src)
      * so that the images can be accessed from the browser while the dev server is running.
      */
-    tempDirname: string,
+    tempDirname?: string,
 
     /**
      * Regular expression that retrieves URLs of images in HTML.
      * Match (the image url) must be on index 0.
      */
-    regexp: RegExp
+    regexp?: RegExp
 }
