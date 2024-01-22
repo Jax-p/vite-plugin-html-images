461c41331b96ff2b13fbcc3f4babec82e48cc32c
Kanti <kanti@kuijk.be>
2024-01-22 23:23:13 +0100

    Revert "Bringing up to date"
    This reverts commit 7f2997044427ec78fbb66a3ad2c683552c73b886.

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,19 @@
 import * as sharp from "sharp";
-import { OutputOptions } from "sharp";
+import {OutputOptions} from "sharp";
 
-export type OptimizationOptions = Record<keyof sharp.FormatEnum, OutputOptions>;
+export type OptimizationOptions = Record<keyof sharp.FormatEnum, OutputOptions>
 
-export interface ImageOptions {
-  /**
-   * The temporary directory must be inside the source directory (src)
-   * so that the images can be accessed from the browser while the dev server is running.
-   */
-  tempDirname?: string;
+export interface ImageOptions
+{
+    /**
+     * The temporary directory must be inside the source directory (src)
+     * so that the images can be accessed from the browser while the dev server is running.
+     */
+    tempDirname?: string,
 
-  /**
-   * Regular expression that retrieves URLs of images in HTML.
-   * Match (the image url) must be on index 0.
-   */
-  regexp?: RegExp;
+    /**
+     * Regular expression that retrieves URLs of images in HTML.
+     * Match (the image url) must be on index 0.
+     */
+    regexp?: RegExp
 }
